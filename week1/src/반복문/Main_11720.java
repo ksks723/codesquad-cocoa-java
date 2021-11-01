3e70ac5ac4f350c4e33664736b77c1d86443aa7a
ksks723 <ksks723@gmail.com>
2021-11-01 18:18:19 +0900

    숫자의 합

--- week1/src/반복문/Main_11720.java
+++ week1/src/반복문/Main_11720.java
@@ -8,6 +8,10 @@ public class Main_11720 {
 
         int a = sc.nextInt();
         String b = sc.next();
+                            //9번 코드에서 입력한 값 + \n 에서
+                            //nextInt()가  입력한 값만 떼어가고 \n 만 남게되는데
+                            //next()는 개행문자(\n)를 무시하고 입력을 받고, 
+                            //nextLine()은 한줄 단위로 입력을 받기 때문에 개행문자도 포함한다
         char[] inputArr = b.toCharArray();
         int sum = 0;
         for(int i=0; i<a; i++){
---
+++ week1/src/별찍기/Main_2439.java
@@ -0,0 +1,10 @@
+package 별찍기;
+
+import java.util.Scanner;
+
+public class Main_2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+    }
+}
