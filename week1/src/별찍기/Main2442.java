e661048147aa03c65642fe4733124c0e160a0bac
ksks723 <ksks723@gmail.com>
2021-11-02 15:32:41 +0900

    feat: 별찍기

--- week1/src/별찍기/Main2442.java
+++ week1/src/별찍기/Main2442.java
@@ -6,7 +6,18 @@ public class Main2442 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-
-
+        sc.close();
+        for(int i= 1; i<=a; i++){
+            for(int j=a; j>i; j--) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            for(int j=1; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
