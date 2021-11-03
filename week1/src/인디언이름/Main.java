1a887add1e7124612945f37b22eee0053f4f07bf
ksks723 <ksks723@gmail.com>
2021-11-03 16:54:51 +0900

    feat: 입력받는기능

--- week1/src/인디언이름/Main.java
+++ week1/src/인디언이름/Main.java
@@ -1,7 +1,19 @@
 package 인디언이름;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+       String[] birth = getBirth();
+
+    }
+
+    public static String[] getBirth() throws IOException {
+        BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("생년월일을 입력해 주세요 ex)1999 07 23");
+        String[] birth = re.readLine().split(" ");
+        return birth;
     }
 }
