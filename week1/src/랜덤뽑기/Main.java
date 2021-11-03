8f8ae8d797d01669b1a87a939e830d9c81b6c7a0
ksks723 <ksks723@gmail.com>
2021-11-03 23:34:36 +0900

    refactor : 정렬

--- week1/src/랜덤뽑기/Main.java
+++ week1/src/랜덤뽑기/Main.java
@@ -5,50 +5,53 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      int answer = getNum();
-      if(ckNum(answer) == 1) {
-         answer = getNum();
-      }
-      String[] pickMem = getMember(answer);
-      printMember(pickMem);
-   }
-   public static int getNum() throws IOException {
-      BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
-      System.out.println("몇 명인가요? :");
-      int answer = Integer.parseInt(re.readLine());
-      return answer;
-   }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int answer = getNum();
+        if (ckNum(answer) == 1) {
+            answer = getNum();
+        }
+        String[] pickMem = getMember(answer);
+        printMember(pickMem);
+    }
 
-   public static int ckNum(int n) {
-      if(n<11 && n>0){
-         return 0;
-      }
-      System.out.println("1~10 사이의 숫자만 입력해주세요 ");
-      return 1;
-   }
-   public static String[] getMember(int n){
-      String[] member = {"검봉","Hanse","Yan","Ella","Konda","sonny","반스","Teemo","Bart","Jarry"};
-      Random rd = new Random();
-      String[] pickMem = new String[n];
-      for(int i = 0; i < n; i++){
-         pickMem[i] = member[rd.nextInt(10)];
-         for(int j = 0; j < i; j++){
-          if(pickMem[i].equals(pickMem[j])){
-               i--;
+    public static int getNum() throws IOException {
+        BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("몇 명인가요? :");
+        int answer = Integer.parseInt(re.readLine());
+        return answer;
+    }
+
+    public static int ckNum(int n) {
+        if (n < 11 && n > 0) {
+            return 0;
+        }
+        System.out.println("1~10 사이의 숫자만 입력해주세요 ");
+        return 1;
+    }
+
+    public static String[] getMember(int n) {
+        String[] member = {"검봉", "Hanse", "Yan", "Ella", "Konda", "sonny", "반스", "Teemo", "Bart", "Jarry"};
+        Random rd = new Random();
+        String[] pickMem = new String[n];
+        for (int i = 0; i < n; i++) {
+            pickMem[i] = member[rd.nextInt(10)];
+            for (int j = 0; j < i; j++) {
+                if (pickMem[i].equals(pickMem[j])) {
+                    i--;
+                }
             }
-         }
-      }
-      return pickMem;
-   }
-   public static void printMember(String[] pickMem){
-      for(int i = 0; i < pickMem.length; i++){
-         System.out.print(pickMem[i]);
-         if(i != pickMem.length-1){
-            System.out.print(", ");
-         }
-      }
-   }
+        }
+        return pickMem;
+    }
+
+    public static void printMember(String[] pickMem) {
+        for (int i = 0; i < pickMem.length; i++) {
+            System.out.print(pickMem[i]);
+            if (i != pickMem.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
 
 }
