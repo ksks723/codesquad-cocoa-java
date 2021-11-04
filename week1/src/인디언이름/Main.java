a5613f92720750883f89823a9c07702fe3d041c4
ksks723 <ksks723@gmail.com>
2021-11-04 23:51:39 +0900

    feat: 이름출력

--- week1/src/인디언이름/Main.java
+++ week1/src/인디언이름/Main.java
@@ -3,13 +3,14 @@ package 인디언이름;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Random;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String[] userBirth = getBirth().split(" ");
-
+        int[] userBirthInt = birthManufacture(userBirth);
+        //NameFactory nf = makeIndiansName(userBirthInt);
+        //printName(nf);
     }
 
     public static String getBirth() throws IOException {
@@ -27,27 +28,25 @@ public class Main {
         return Pattern.matches(pattern, userBirth);
     }
 
-
-    public static String getYearName(int n) {
-        String[] name = {"시끄러운", "푸른", "어두운", "조용한", "웅크린", "백색",
-                "지혜로운", "용감한", "날카로운", "욕심많은"};
-
-        return "";
+    public static int[] birthManufacture(String[] userBirth) {
+        int year = Integer.parseInt(userBirth[0].substring(3, 3));
+        int month = Integer.parseInt(userBirth[1]);
+        int day = Integer.parseInt(userBirth[2]);
+        int[] userBirthInt = new int[]{year, month, day};
+        for(int i : userBirthInt){
+            System.out.println(i);
+        }
+        return userBirthInt;
     }
 
-    public static String getMonthName(int n) {
-        String[] name = {"늑대", "태양", "양", "매", "황소", "불꽃",
-                "나무", "달빛", "말", "돼지", "하늘", "바람"};
-
-        return "";
+    public static NameFactory makeIndiansName(int[] userBirth) {
+        NameFactory nf = new NameFactory(userBirth[0],userBirth[1],userBirth[2]);
+        return nf;
     }
 
-    public static String getDayName(int n) {
-        String[] name = {"와(과) 함께 춤을", "의 기상", "은(는) 그림자 속에", "", "", "", "의 환생", "의 죽음", "아래에서",
-                "을(를) 보라", "이(가) 노래하다.", "의 그늘 → 그림자", "의 일격", "에게 쫒기는 남자", "의 행진", "의 왕", "의 유령",
-                "을 죽인 자.", "은(는) 맨날 잠잔다.", "처럼..", "의 고향", "의 전사", "은(는) 나의 친구", "의 노래",
-                "의 정령", "의 파수꾼", "의 악마", "와(과) 같은 사나이", "의 심판자→을(를) 쓰러뜨린 자", "의 혼", "은(는) 말이 없다."};
-
-        return "";
+    public static void printName(NameFactory nf) {
+        System.out.println(nf);
     }
+
+
 }
